6188de789925728fd13e0b066aa6b7b3c9df7d19
LPCSD <luis.p.durao@gmail.com>
2016-10-20 20:55:10 +0100

    Configuration CSV interpreter added

--- Factory Controller/src/main/Configuration.java
+++ Factory Controller/src/main/Configuration.java
@@ -17,14 +17,48 @@ import java.util.logging.Logger;
 public class Configuration {
     
     private final Properties general = new Properties();
-    private final ??? io = ???;
+    public final Map<String, Integer> inputIds = new HashMap<>();
+    public final Map<String, Integer> outputIds = new HashMap<>();
+    private static final String CSVFileName = "io.csv";
     
     Configuration() {
         try { general.load(new FileInputStream(new File("src/main/config.properties"))); }
         catch (Exception ex) { Logger.getLogger(Configuration.class.getName()).log(Level.SEVERE, null, ex); }
         
+        List<String> errorList = new ArrayList<>();
+        
         // Load io map
-        io = 
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader(CSVFileName));
+            String line;
+            for( int lineNumber=1; (line = br.readLine()) != null; lineNumber++)
+            {
+                String[] values = line.split(",");
+                if(values.length != 3)
+                    throw new Exception("Line " + lineNumber + " doesn't have 3 values.");
+                //decide if it is an input or and output
+                switch(values[1])
+                {
+                    case "I":
+                        inputIds.put(values[0],Integer.parseInt(values[2]));
+                        break;
+                    case "O":
+                        outputIds.put(values[0], Integer.parseInt(values[2]));
+                        break;
+                    default:
+                      throw new Exception("Invalid I/O classifier \"" + values[1] + "\" at line " + lineNumber);  
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            System.err.println( "Error while opening/reading the csv file." + "\n" +
+                                "Exception msg: " + e.getMessage() + "\n" +
+                                "Program will exit now.");
+            try{System.in.read();}catch(Exception ignored){}
+            System.exit(10);
+        }
     }
     
     public String getS(String key) {
@@ -37,10 +71,10 @@ public class Configuration {
     
     
     public int getBaseInput(String id) {
-        return 0;
+        return inputIds.getOrDefault(id, -1);
     }
     
     public int getBaseOutput(String id) {
-        return 0;
+        return outputIds.getOrDefault(id, -1);
     }
 }
