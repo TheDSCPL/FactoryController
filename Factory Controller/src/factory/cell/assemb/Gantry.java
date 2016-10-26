b1d466abd7a933cd7d92007a83412611bed0014c
LPCSD <luis.p.durao@gmail.com>
2016-10-26 13:14:45 +0100

    Gantry now initializes even if out of sensor bounds and main now starts the simulator if it isn't running.

--- Factory Controller/src/factory/cell/assemb/Gantry.java
+++ Factory Controller/src/factory/cell/assemb/Gantry.java
@@ -20,7 +20,9 @@ public class Gantry
     
     //Inputs
     public final Sensor maxX,minX,upZ,downZ;
-    public final Sensor[] Ysensors;
+    public final Sensor[] Xsensors, Ysensors;
+
+    private final long initializationTimeoutMillis = 1500;
     
     public final String id;
     /**
@@ -29,6 +31,19 @@ public class Gantry
     private boolean gripState = false;
     private final int gripOutputId;
     public final Sensor presenceSensor;
+    /**
+     * including spaces between sensors (space above sensor 0 at position 0, sensor 0 at position 1, space between sensor 0 and sensor 1 at position 2, sensor 1 at position 3, ...)
+     * -1 means undefined position
+     */
+    private int isAtX;
+    /**
+     * including spaces between sensors (space above sensor 0 at position 0, sensor 0 at position 1, space between sensor 0 and sensor 1 at position 2, sensor 1 at position 3, ...)
+     * -1 means undefined position
+     */
+    private int isAtY;
+    
+    private XYSTATE XState = XYSTATE.INITIALIZING1ST,
+                    YState = XYSTATE.INITIALIZING1ST;
     
     Gantry(String id)
     {
@@ -46,6 +61,7 @@ public class Gantry
         Sensor y0, y1, y2, y3, y4;
         minX  = new Sensor(inputSensorId + 0);
         maxX  = new Sensor(inputSensorId + 1);
+        Xsensors = new Sensor[] {minX,maxX};
         y0    = new Sensor(inputSensorId + 2);
         y1    = new Sensor(inputSensorId + 3);
         y2    = new Sensor(inputSensorId + 4);
@@ -56,6 +72,147 @@ public class Gantry
         upZ   = new Sensor(inputSensorId + 8);
         
         presenceSensor = new Sensor(inputSensorId);
+    }
+    
+    private long initXTimer = -1, initYTimer = -1;
+    
+    /**
+     * @return The Xsensor that is active or -1 if none are active
+     */
+    private int getActiveXSensor()
+    {
+        for(int s = 0; s < Xsensors.length; s++)
+            if(Xsensors[s].on())
+                return s;
+        return -1;
+    }
+    
+    /**
+     * @return The Ysensor that is active or -1 if none are active
+     */
+    private int getActiveYSensor()
+    {
+        for(int s = 0; s < Ysensors.length; s++)
+            if(Ysensors[s].on())
+                return s;
+        return -1;
+    }
+     
+    private enum XYSTATE
+    {
+        INITIALIZING1ST, INITIALIZING2ND, IDLE, MOVINGPLUS, MOVINGMINUS;
+    }
+    
+    /**
+     * Updates <i>isAtX</i> and <i>isAtY</i> variables.
+     * <b>It's asured that this method only changes the XY state machines if they are initializing.</b> (so you don't have to worry about this changing the states after the initialization is done)
+     * @return wether (or not) the gantry is initializing at the start of this update cycle
+     */
+    private boolean updateIsAt()
+    {
+        //I want the return to depend only at the previous state so I evaluate it here (at the start)
+        boolean ret = (XState == XYSTATE.INITIALIZING1ST || XState == XYSTATE.INITIALIZING2ND || YState == XYSTATE.INITIALIZING1ST || YState == XYSTATE.INITIALIZING2ND);
+        //---------------UPDATE isAtX-----------------
+        if(getActiveXSensor() != -1)    //is at a sensor
+            if( XState == XYSTATE.INITIALIZING1ST || XState == XYSTATE.INITIALIZING2ND /*isAtX == -1*/) //i'm initializing and found a sensor
+            {
+                isAtX = getActiveXSensor()*2+1 ; //for example, sensor 1 is at position 3
+                XMotor.turnOff();
+                XState = XYSTATE.IDLE;
+            }
+            else
+            {
+                isAtX = getActiveXSensor()*2+1 ; //for example, sensor 1 is at position 3
+            }
+        else    //is in a space
+            switch(XState)   //evaluates last cycle's isAtX
+            {
+                case INITIALIZING1ST:    //if undefined position; 1st attempt
+                    if(initXTimer == -1)
+                    {
+                        initXTimer = System.currentTimeMillis();
+                        XMotor.turnOnMinus();
+                    }
+                    else if(System.currentTimeMillis() - initXTimer >= initializationTimeoutMillis)
+                    {
+                        XState = XYSTATE.INITIALIZING2ND;
+                        initXTimer = System.currentTimeMillis();
+                        XMotor.turnOff();
+                    }
+                break;
+                case INITIALIZING2ND:    //if undefined position; 2nd attempt
+                    if(System.currentTimeMillis() - initXTimer >= initializationTimeoutMillis)
+                    {
+                        //If the gatry is now trying to initialize in the other direction (second attempt) and it times out, then it's an error
+                        throw new Error("Gantry initialization timed out");
+                    }
+                    XMotor.turnOnPlus();
+                break;
+                case IDLE:
+                    XMotor.turnOff();
+                break;
+                default:
+                    if( isAtX % 2 != 0 ) //previous state was a sensor, we have to decide which space it went to
+                        isAtX = isAtX + ( XState == XYSTATE.MOVINGPLUS ? 1 : -1); //if moving in the positive direction, to go the next space, else go to the previous space
+            }
+        
+        
+        //---------------UPDATE isAtX-----------------
+        if(getActiveYSensor() != -1)    //is at a sensor
+            if( YState == XYSTATE.INITIALIZING1ST || YState == XYSTATE.INITIALIZING2ND /*isAtX == -1*/) //i'm initializing and found a sensor
+            {
+                isAtY = getActiveYSensor()*2+1 ; //for example, sensor 1 is at position 3
+                YMotor.turnOff();
+                YState = XYSTATE.IDLE;
+            }
+            else
+            {
+                isAtY = getActiveYSensor()*2+1 ; //for example, sensor 1 is at position 3
+            }
+        else    //is in a space
+            switch(YState)   //evaluates last cycle's isAtX
+            {
+                case INITIALIZING1ST:    //if undefined position; 1st attempt
+                    if(initYTimer == -1)
+                    {
+                        initYTimer = System.currentTimeMillis();
+                        YMotor.turnOnMinus();
+                    }
+                    else if(System.currentTimeMillis() - initYTimer >= initializationTimeoutMillis)
+                    {
+                        YState = XYSTATE.INITIALIZING2ND;
+                        initYTimer = System.currentTimeMillis();
+                        YMotor.turnOff();
+                    }
+                break;
+                case INITIALIZING2ND:    //if undefined position; 2nd attempt
+                    if(System.currentTimeMillis() - initYTimer >= initializationTimeoutMillis)
+                    {
+                        //If the gatry is now trying to initialize in the other direction (second attempt) and it times out, then it's an error
+                        throw new Error("Gantry initialization timed out");
+                    }
+                    YMotor.turnOnPlus();
+                break;
+                case IDLE:
+                    YMotor.turnOff();
+                break;
+                default:
+                    if( isAtY % 2 != 0 ) //previous state was a sensor, we have to decide which space it went to
+                        isAtY = isAtY + ( YState == XYSTATE.MOVINGPLUS ? 1 : -1); //if moving in the positive direction, to go the next space, else go to the previous space
+            }
+        return ret;
+    }
+    
+    void update()
+    {
+        //Just updates the isAtX/Y variables
+        try{if(updateIsAt())
+            return;}
+        finally
+        {
+            System.out.println("Gantry: XState = " + XState.name() + " YState = " + YState.name());
+        }
+        
     }
     
     public boolean hasBlock()
@@ -76,5 +233,12 @@ public class Gantry
     public boolean isOpen()
     {
         return !gripState;
+    }
+    
+    void tranferBlock(int fromX, int fromY, int toX, int toY)
+    {
+        if( (fromX > 1 || fromX <0) || (toX > 1 || toX < 0) || (fromY > 4 || fromY < 0) || (toY > 4 || toY < 0) )
+            throw new IndexOutOfBoundsException("Tried to transfer a block with Gantry from/to invalid coordinates");
+        
     }
 }
--- Factory Controller/src/main/Main.java
+++ Factory Controller/src/main/Main.java
@@ -9,6 +9,8 @@ import coms.*;
 import control.*;
 import factory.*;
 
+//TODO: create a class Error extends java.lang.Error that, in the constructor, has a printStackTrace and a System.exit so it is a fatal error and can't be caught.
+
 /**
  *
  * @author Alex
@@ -21,22 +23,56 @@ public class Main {
     public static final TransformationManager transfm = new TransformationManager();
     public static final OrderController orderc = new OrderController();
 
+    private static void connectAndRun() throws Exception
+    {
+        modbus.connect();
+
+        while (true) {
+            modbus.refreshInputs();
+            factory.update();
+            orderc.update();
+            modbus.refreshOutputs();
+
+            Thread.sleep(1);
+        }
+    }
+    
+    private static Process sfs = null;
+    private static Thread killSFS = null;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try {
-            modbus.connect();
-            
-            while (true) {
-                modbus.refreshInputs();
-                factory.update();
-                orderc.update();
-                modbus.refreshOutputs();
+        try { connectAndRun(); }
+        catch (java.net.ConnectException ex)    //if simulator not running try to start it
+        {
+            System.err.println("Simulator not running. Trying to start it.");
+            try
+            {
+                sfs = Runtime.getRuntime().exec("java -jar sfs.jar");
                 
-                Thread.sleep(1);
+                if(!sfs.isAlive())
+                    throw new Error("Error starting simulator. Check permissions and if all of the simulation's files are present.");
+                Thread.sleep(100);
+                connectAndRun();
             }
-
-        } catch (Exception ex) { ex.printStackTrace(); } 
+            catch(Error e)
+            {
+                e.printStackTrace();
+                return;
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+                throw new Error("Error starting simulator. Check permissions and if all of the simulation's files are present.");
+            }
+        }
+        catch(Error e)
+        {
+            e.printStackTrace();
+        }
+        catch (Exception ex)
+        { ex.printStackTrace(); } 
     }
 }
