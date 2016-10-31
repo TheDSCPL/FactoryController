a5e47ab8aa47c09e4761c1536ea70ea6be959298
Alexandre Truppel <alex.truppel@gmail.com>
2016-10-31 13:05:47 +0000

    Assembler testing in progress

--- Factory Controller/src/factory/Motor.java
+++ Factory Controller/src/factory/Motor.java
@@ -12,51 +12,57 @@ import main.*;
  * @author Alex
  */
 public class Motor {
-    
+
     public final int baseIndex;
-    
+
     public Motor(int baseIndex) {
         this.baseIndex = baseIndex;
     }
-    
+
     /**
      * Controls the motor.
-     * @param on represents the state of the motor. <i>true</i> if it is moving and <i>false</i> if it is stopped
-     * @param plus represents the way in which the motor is moving. <i>true</i> if moving in the positive way and <i>false</i> if moving in the negative way
+     *
+     * @param on represents the state of the motor. <i>true</i> if it is moving
+     * and <i>false</i> if it is stopped
+     * @param plus represents the way in which the motor is moving. <i>true</i>
+     * if moving in the positive way and <i>false</i> if moving in the negative
+     * way
      */
     public void control(boolean on, boolean plus) {
         Main.modbus.setOutput(baseIndex, on && plus);
         Main.modbus.setOutput(baseIndex + 1, on && !plus);
-        /*if(on)
-        {
-            if(baseIndex==169)
-                System.out.println("X" + (plus?"+":"-") );
-            if(baseIndex==171)
-                System.out.println("Y" + (plus?"+":"-") );
+        if (on) {
+            if (baseIndex == 169) {
+                System.out.println("X" + (plus ? "+" : "-"));
+            }
+            if (baseIndex == 171) {
+                System.out.println("Y" + (plus ? "+" : "-"));
+            }
         }
-        else
-        {
-            if(baseIndex==169)
+        else {
+            if (baseIndex == 169) {
                 System.out.println("Xoff");
-            if(baseIndex==171)
+            }
+            if (baseIndex == 171) {
                 System.out.println("Yoff");
-        }*/
+            }
+        }
     }
-    
+
     public void turnOff() {
         control(false, false);
     }
-    
+
     public void turnOn(boolean plus) {
         control(true, plus);
     }
-    
+
     public void turnOnPlus() {
         turnOn(true);
     }
-    
+
     public void turnOnMinus() {
         turnOn(false);
     }
-    
+
 }
