e5fb1ea89819c459b07abc76e79928e8476ec6b6
Alexandre Truppel <alex.truppel@gmail.com>
2016-11-21 23:36:01 +0000

    Minor changes

--- Factory Controller/src/factory/BlockContainer.java
+++ Factory Controller/src/factory/BlockContainer.java
@@ -4,5 +4,5 @@ package factory;
  * Superclass of classes that can contain blocks
  */
 public abstract class BlockContainer {
-    
+    // TODO: refactor into having blocks here instead of conveyor
 }
