9e051b09a74be088f067b8a05d0afd1d402761b3
Alexandre Truppel <alex.truppel@gmail.com>
2016-12-07 13:23:59 +0000

    Corrected mistake in las mega-merge

--- Factory Controller/src/main/Main.java
+++ Factory Controller/src/main/Main.java
@@ -77,6 +77,7 @@ public class Main {
                 modbus.refreshInputs();
                 factory.update();
                 orderc.update();
+                optimizer.distributeNextOrder();
                 modbus.refreshOutputs();
 
                 processCommand();
